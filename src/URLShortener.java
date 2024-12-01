75005b9c1c830a3cd23bbd250fb91f83a465d779
Kadyrov Mikhail <mihailkadyrov2002@mail.ru>
2024-12-01 15:17:06 +0300

    Commited 2 options (modify ttl & noc)

--- src/URLShortener.java
+++ src/URLShortener.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.UUID;
 
